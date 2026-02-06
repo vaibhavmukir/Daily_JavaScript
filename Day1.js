b425eda7ef06a0f16a0faf156b08fc29e587ef66
Vaibhav Mukir <mukirvaibhav@gmail.com>
2026-02-06 15:43:17 +0530

    Variable

--- Day1.js
+++ Day1.js
@@ -1,1 +1,19 @@
+//Day 1
+
 console.log("Hello JavaScript");
+
+const accountId=1233; //not Changeble
+
+var accountPassword=1234;
+/*
+Prefer not to use var
+Becouse of issue in block scope and functional scope
+*/
+
+let accountEmail="vaibhav@123"; //safe use block scope varibale
+
+accountCity ="Pune";
+
+let mob_no; // javascrpit jab bhi hamne value nahi assign kiyi to undifed krta hai...
+
+console.table([accountId,accountPassword,accountEmail,accountCity,mob_no]); // this is all value the print in the table
