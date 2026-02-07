3fed3ee7ba6051e6580678f320ddf48645216cc5
Vaibhav Mukir <mukirvaibhav@gmail.com>
2026-02-07 14:36:16 +0530

    Data type in js

---
+++ Day2.js
@@ -0,0 +1,25 @@
+"use stict" //treat all JS code ans never as newer version
+
+//Data Types
+
+let name="Vaibhav" //String
+
+let age=32;  //Numebr
+
+let isLoggdIn=false //Boolean
+
+//number => 2 to 53
+//bigint
+
+//String => ""
+//boolean = true or false
+//null  => standAloan value .....null is the Object 
+//undedind => 
+//symbol => uniqueness use
+
+//Object
+
+console.log(typeof age) //Cheking the value wich type
+
+console.log(typeof undefined) // type of Undeifend
+console.log(typeof null)  // type of Object
