467ab0ada8f3574da38ee096f27ea23aacd90509
Vaibhav Mukir <mukirvaibhav@gmail.com>
2026-02-07 14:52:44 +0530

    Conversion

---
+++ Day3.js
@@ -0,0 +1,17 @@
+// Conuversion and Operation
+
+let score="33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueIntNumber = Number(score)
+console.log(typeof valueIntNumber)// Conversion of value
+
+// "33" => 33
+
+// "33dd" =>NaN
+
+// true => 1
+
+// false => 0
